27ac59653353d948d06ceca3f768ff0a137ffbe5
casperjakobsen <jakobsen.casper@gmail.com>
2019-04-27 18:11:46 +0200

    Music

---
+++ LudumDare44/Assets/Scripts/Music.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    [SerializeField] float fadeInTime = 1f;
+    [SerializeField] float maxVolume = 1f;
+    [SerializeField] AnimationCurve fadeInCurve;
+
+    public static bool instantiated;
+    AudioSource audio;
+    void Awake()
+    {
+        if (instantiated)
+        {
+            Destroy(gameObject);
+        }
+        audio = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
+        instantiated = true;
+    }
+
+    void Update()
+    {
+        if (Time.time < fadeInTime)
+        {
+            audio.volume = fadeInCurve.Evaluate(Time.time/fadeInTime) * maxVolume;
+        }
+        else
+        {
+            audio.volume = maxVolume;
+        }
+    }
+}
