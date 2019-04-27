c3f6c2108d2dce8f418b9c116f6b17e7707f1f99
casperjakobsen <jakobsen.casper@gmail.com>
2019-04-27 16:04:30 +0200

    Menu and buttons

---
+++ LudumDare44/Assets/Scripts/TileButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum ButtonType
+{
+    Play, Exit
+}
+
+public class TileButton : MonoBehaviour
+{
+    [SerializeField] Sprite idleSprite;
+    [SerializeField] Sprite hoverSprite;
+    [SerializeField] Sprite downSprite;
+    [SerializeField] ButtonType type;
+
+    SpriteRenderer spriteRenderer;
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseEnter()
+    {
+        spriteRenderer.sprite = hoverSprite;
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.sprite = idleSprite;
+    }
+
+    private void OnMouseDown()
+    {
+        spriteRenderer.sprite = downSprite;
+        switch (type)
+        {
+            case ButtonType.Play:
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            case ButtonType.Exit:
+                Application.Quit();
+                break;
+        }
+    }
+}
