f22b7cb69d7c96f181e43da2a62d89e0f65842b7
casperjakobsen <jakobsen.casper@gmail.com>
2019-04-27 08:49:15 +0200

    Basic player movement

---
+++ LudumDare44/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Tilemap tilemap;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3Int movement = Vector3Int.zero;
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            movement = Vector3Int.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            movement = Vector3Int.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            movement = Vector3Int.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            movement = Vector3Int.left;
+        }
+
+        Vector3Int currentPos = tilemap.WorldToCell(transform.position);
+        if (tilemap.GetTile(currentPos + movement) == null)
+        {
+            transform.position += movement;
+        }
+    }
+}
