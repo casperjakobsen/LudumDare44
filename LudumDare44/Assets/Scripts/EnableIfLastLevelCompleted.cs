828a5d9c9450f5f2d2ef2fa97e152a7b891e8824
casperjakobsen <jakobsen.casper@gmail.com>
2019-04-29 00:21:14 +0200

    Minor fix

--- LudumDare44/Assets/Scripts/EnableIfLastLevelCompleted.cs
+++ LudumDare44/Assets/Scripts/EnableIfLastLevelCompleted.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class EnableIfLastLevelCompleted : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        if (PlayerPrefs.HasKey("Progress") && PlayerPrefs.GetInt("Progress") == GameController.lastLevel)
-        {
-            enabled = true;
-        }
-        else
-        {
-            enabled = false;
-        }
+        gameObject.SetActive(PlayerPrefs.HasKey("Progress") && PlayerPrefs.GetInt("Progress") == GameController.lastLevel);
     }
 }
